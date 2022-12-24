52942c06da3621c111bb1a646ec228c0de153780
Laishyde <laiscristina.animamundi@gmail.com>
2022-12-24 01:41:56 -0300

    feat inicio

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const cat = document.querySelector('.cat');
+const pipe = document.querySelector('.pipe');
+
+const jump = () =>{
+    cat.classList.add('jump');
+    setTimeout(()=>{
+        cat.classList.remove('jump');
+
+    }, 500);
+
+}
+const loop = setInterval(() =>{
+    const pipePosition = pipe.offsetLeft;
+    const catPosition = +window.getComputedStyle(cat).bottom.replace('px','');
+  if(pipePosition <= 44 && pipePosition > 0 && catPosition < 110){
+    pipe.style.animation = 'none';
+    pipe.style.left = `${pipePosition}px`;
+
+      cat.style.animation = 'none';
+      cat.style.bottom = `${catPosition}px`;
+      cat.style.width = '75px'
+      cat.src = './assets/imagens/gato-bravo.gif'
+      clearInterval(loop);
+    
+  }
+  
+
+},10);
+
+
+document.addEventListener('click',jump);
